c3d2a573bf84ee73a2cd94d1540abeda7aeabdbf
ajp8164 <andyphillipson@gmail.com>
2019-02-07 22:23:18 -0500

    Replace http, bump version 0.0.4

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 var explorerLib = {};
 
 explorerLib.version = 'v' + require('./package.json').version;
-explorerLib.Explorer = require('./lib/explorer');
+explorerLib.Explorer = require('./lib/model/explorer');
 
 module.exports = explorerLib;
